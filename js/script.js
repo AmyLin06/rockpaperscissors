fbf51a27b217614a056169546fb13fcf67c9abee
Amy <AmyLin1928@gmail.com>
2019-11-06 23:22:57 +0000

    completed ranom number computer generator

--- js/script.js
+++ js/script.js
@@ -9,3 +9,10 @@
 
 // DOCUMENT READY FUNCTION BELOW
 
+$.("#shoot").click(function(){
+    var input = $("#input").val();
+    var choices = ["rock" , "paper" , "scissors"];
+    var index = Math.floor(Math.random() * 2);
+    var computerResults = choices[index];
+})
+
